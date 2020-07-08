3b775879d2dc91512fd6b15ee7609d68f05c9d68
GiancarmeloPittala <56794422+GiancarmeloPittala@users.noreply.github.com>
2020-07-09 01:58:07 +0200

    fix security

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const
   { parsed : { PORT } } = require('dotenv').config(),
   bodyParser = require('body-parser'),
   morgan = require('morgan'),
-  { sequelize } = require('./models');
+  { sequelize } = require('./models'),
+  helmet = require('helmet')
 
+
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
